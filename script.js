e685fe3280d82b2b927364f54849f966d4c0142d
Open Home Automation <contact@openhomeautomation.net>
2013-05-29 16:20:59 +0000

    initial commit of PHP and js files

---
+++ script.js
@@ -0,0 +1,92 @@
+window.onload = function(){
+	buttonOn = document.getElementById('submitOn');
+	buttonOn.onClick = relayOn;
+	
+	buttonOff = document.getElementById('submitOff');
+	buttonOff.onClick = relayOff;
+}
+
+function relayOn(){
+	
+	hidden = document.getElementById("total");
+	hidden.value = "on";
+	
+	form = document.getElementById("relay");
+	form.method = "GET";
+	form.action = "remote_relay.php";
+	form.submit();
+}
+
+function relayOff(){
+	
+	hidden = document.getElementById("total");
+	hidden.value = "off";
+	
+	form = document.getElementById("relay");
+	form.method = "GET";
+	form.action = "remote_relay.php";
+	form.submit();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
