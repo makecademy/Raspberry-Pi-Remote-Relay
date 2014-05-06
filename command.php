c6b305b575b58ed6dee5328a13a04aa523d39d8e
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-05-06 14:53:09 +0200

    exec to system

--- command.php
+++ command.php
@@ -1,13 +1,13 @@
 <?php
 
-	exec("sudo gpio -g mode 4 out");
+	system("sudo gpio -g mode 4 out");
 	$state = $_GET["state"];
 	  
 	if ($state == "on"){
-		exec("sudo gpio -g write 4 1");
+		system("sudo gpio -g write 4 1");
 	}
 	if ($state == "off"){
-		exec("sudo gpio -g write 4 0");
+		system("sudo gpio -g write 4 0");
 	}
 	
 ?>
